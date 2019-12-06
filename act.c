83b514d7ca4b317cfdf817406f80a30b89e3cbee
RameshwarPatil <58586331+RameshwarPatil@users.noreply.github.com>
2019-12-06 18:18:08 +0530

    Create act.c

---
+++ act.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+
+void sumarr(const int* parr, int n);
+void display(const int parr,int n);
+
+int main() {
+  int i,n=10;
+  int arr[10];
+  fill(arr,n);
+  sumarr(arr,n);
+  display(arr,n);
+  
+  return 0;
+}
+
+void fill(int* parr,int n) 
+{
+  for(i=0;i<n;i++)
+    parr[i]=i*10+1; 
+}
+
+void sumarr(const int* parr, int n) {
+  int sum=0;
+  for(i=0;i<n;i++)
+    sum+=*parr++;  
+}
+
+void display(const int *parr,int n) {
+  for(i=0;i<n;i++)
+    printf("%d\t",parr[i]);
+}
