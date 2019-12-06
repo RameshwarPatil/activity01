f41236e5a77d9dcf1d3de1f2f8b929ac4574013d
RameshwarPatil <58586331+RameshwarPatil@users.noreply.github.com>
2019-12-06 18:35:12 +0530

    Update act.c

--- act.c
+++ act.c
@@ -5,7 +5,7 @@ void sumarr(const int* parr, int n);
 void display(const int *parr,int n);
 
 int main() {
-  int i,n=10;
+  int n=10;
   int arr[10];
   fill(arr,n);
   sumarr(arr,n);
@@ -20,7 +20,8 @@ void fill(int* parr,int n)
     parr[i]=i*10+1; 
 }
 
-void sumarr(const int *parr, int n) {
+void sumarr(const int *parr, int n)
+{
   int sum=0;
   for(int i=0;i<n;i++)
     sum+=*parr++;  
